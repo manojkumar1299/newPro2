031930e38f1a216c1d96edf7cac69ecef7a27090
manojkumar1299 <81214001+manojkumar1299@users.noreply.github.com>
2022-03-03 16:42:55 +0530

    Update A.java

--- A.java
+++ A.java
@@ -2,5 +2,7 @@ public class A {
  public static void main(String args[]){
 System.out.println("Hello Jenlins have a good day");
 System.out.println("Hello i am coming");
+ System.out.println("Hello ing");
+
 }
 }
